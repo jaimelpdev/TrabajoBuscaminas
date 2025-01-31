675d492e6de1b00e4e10075cf0959e14681a665f
jaimelpdev <129200382+jaimelpdev@users.noreply.github.com>
2025-01-31 18:44:50 +0100

    Comments to do at start

---
+++ html/script.js
@@ -0,0 +1,5 @@
+// Hacer una funcion para el tablero 10x10
+
+// Una funcion que cree las minas aleatoriamente del tablero (10 minas)
+
+// Una funcion que cuente las minas alrededor de una casilla (10 minas)
